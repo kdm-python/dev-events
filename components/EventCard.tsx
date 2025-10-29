900653f86d429d54a9e256a774dc4b1e14cc6132
Kyle Marden <demolaze@Mac.home>
2025-10-29 13:31:43 +0000

    Database models and functions implemented

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -20,20 +20,20 @@ const EventCard = ({ title, image, slug, location, date, time }: Props) => {
         height={300}
         className="poster"
       />
+      <p className="title">{title}</p>
 
       <div className="flex flex-row gap-2">
         <Image src="/icons/pin.svg" alt="location" width={14} height={14} />
         <p>{location}</p>
       </div>
 
-      <p className="title">{title}</p>
-
-      <div className="datetime">
-        <div>
+      <div className="datetime flex gap-4">
+        <div className="flex items-center gap-2">
           <Image src="/icons/calendar.svg" alt="date" width={14} height={14} />
           <p>{date}</p>
         </div>
-        <div>
+
+        <div className="flex items-center gap-2">
           <Image src="/icons/clock.svg" alt="time" width={14} height={14} />
           <p>{time}</p>
         </div>
